4d79810731c6c4a81fbc8d6d3139e854e52389b8
Haahns <jakob.hahnsson@gmail.com>
2021-05-19 18:44:50 +0300

    helloworld

---
+++ worlddata.js
@@ -0,0 +1,9 @@
+function worlddata(){
+    d3.csv("country_profile_variables.csv", function(data) {
+    console.log(data[2].Region);
+        /*for (var i = 0; i < 20; i++) {
+        console.log(data[i].Country);
+        console.log(data[i].Value);
+    }*/
+});
+}
