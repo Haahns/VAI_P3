520ee9044665ab51a34780b539de9448e751008f
Haahns <jakob.hahnsson@gmail.com>
2021-05-26 11:22:00 +0300

    final laptop commit

--- worlddata.js
+++ worlddata.js
@@ -1,6 +1,6 @@
 function worlddata(){
-    d3.json("CPV_array.json", function(data) {
-    console.log(data);
+    d3.csv("country_profile_variables.csv", function(data) {
+    console.log(data[2]);
         /*for (var i = 0; i < 20; i++) {
         console.log(data[i].Country);
         console.log(data[i].Value);
